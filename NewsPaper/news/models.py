198edcf5d3f4ca75ca529833cf20c9c988640ebf
Vasilya <lara_nabieva@bk.ru>
2025-02-10 21:01:51 +0400

    Modul 19
    Adding filtering and pagination; Creating, editing, and deleting objects.

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from news.resources import POSITIONS
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
     rating = models.IntegerField(default=0, verbose_name='Рейтинг')
-
+    def __str__(self):
+        return self.user.username
     def update_rating(self):
         #суммарный рейтинг каждой статьи автора умножается на 3;
         post_rating = sum(post.rating for post in self.post_set.all()) * 3
@@ -18,8 +20,11 @@ class Author(models.Model):
         return self.rating
 
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name='Название категории')
+    def __str__(self):
+        return self.name
 class Post(models.Model):
     author=models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='Автор')
     post_type=models.CharField(max_length=7, choices=POSITIONS, verbose_name='Тип')
@@ -40,6 +45,8 @@ class Post(models.Model):
 
     def preview(self):
         return self.text[0:124] + '...'
+    def get_absolute_url(self):
+        return reverse('about_new', kwargs={'pk': self.pk})
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Пост')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
-# Импортируем созданные нами представления
-from .views import NewsList, PostDetail
+from .views import NewsList, PostDetail, PostCreate, PostUpdate, PostDelete
 
 urlpatterns = [
-   # path — означает путь.
-   # В данном случае путь ко всем товарам у нас останется пустым.
-   # Т.к. наше объявленное представление является классом,
-   # а Django ожидает функцию, нам надо представить этот класс в виде view.
-   # Для этого вызываем метод as_view.
     path('', NewsList.as_view(), name='news_list'),
-   # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
-   # int — указывает на то, что принимаются только целочисленные значения
     path('news/<int:pk>/', PostDetail.as_view(), name='about_new'),
+    path('news/create/', PostCreate.as_view(), {'is_article': False}, name='create_news'),
+    path('news/<int:pk>/edit/', PostUpdate.as_view(), name='edit_news'),
+    path('news/<int:pk>/delete/', PostDelete.as_view(), name='delete_news'),
+    path('articles/create/', PostCreate.as_view(), {'is_article': True}, name='create_article'),
+    path('articles/<int:pk>/edit/', PostUpdate.as_view(), name='edit_article'),
+    path('articles/<int:pk>/delete/', PostDelete.as_view(), name='delete_article'),
 ]
